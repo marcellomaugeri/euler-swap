a2a36da1d596597b249a218e6d8a9c07cbeefbf0
Doug Hoyte <doug@hcsw.org>
2025-02-08 00:26:24 -0500

    fmt

--- test/PreserveNav.t.sol
+++ test/PreserveNav.t.sol
@@ -39,9 +39,18 @@ contract PreserveNav is MaglevTestBase {
         maglev.configure();
     }
 
-
-
-    function test_preserve_nav(uint256 cx, uint256 cy, uint256 fee, bool preSkimDir, bool dir1, uint256 amount1, bool skimOrder1, bool dir2, uint256 amount2, bool skimOrder2) public {
+    function test_preserve_nav(
+        uint256 cx,
+        uint256 cy,
+        uint256 fee,
+        bool preSkimDir,
+        bool dir1,
+        uint256 amount1,
+        bool skimOrder1,
+        bool dir2,
+        uint256 amount2,
+        bool skimOrder2
+    ) public {
         cx = bound(cx, 0.1e18, 0.99e18);
         cy = bound(cy, 0.1e18, 0.99e18);
         fee = bound(fee, 0, 0.2e18);
@@ -49,6 +58,7 @@ contract PreserveNav is MaglevTestBase {
         amount2 = bound(amount2, 0.00001e18, 25e18);
 
         if (fee < 0.1e18) fee = 0; // half of the time use fee 0
+
         else fee -= 0.1e18;
 
         createMaglev(50e18, 50e18, fee, 1e18, 1e18, cx, cy);
@@ -92,8 +102,6 @@ contract PreserveNav is MaglevTestBase {
 
         assertGe(getHolderNAV(), nav1);
     }
-
-
 
     function _skimAll(bool dir) public returns (uint256) {
         uint256 skimmed = 0;
