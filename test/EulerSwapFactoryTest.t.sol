578f2f9589f4dc6822cc416f8ddf92ae79c4ae14
saucepoint <saucepoint@protonmail.com>
2025-03-05 23:55:31 -0500

    fix test; added utilities for tests

--- test/EulerSwapFactoryTest.t.sol
+++ test/EulerSwapFactoryTest.t.sol
@@ -2,29 +2,41 @@
 pragma solidity ^0.8.24;
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolManagerDeployer} from "./utils/PoolManagerDeployer.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {HookMiner} from "../src/utils/HookMiner.sol";
 import {EulerSwapTestBase, IEulerSwap, IEVC, EulerSwap} from "./EulerSwapTestBase.t.sol";
 import {EulerSwapFactory, IEulerSwapFactory} from "../src/EulerSwapFactory.sol";
+import {EulerSwapHook} from "../src/EulerSwapHook.sol";
 
 contract EulerSwapFactoryTest is EulerSwapTestBase {
     EulerSwapFactory public eulerSwapFactory;
+    IPoolManager public poolManager;
 
     uint256 minFee = 0.0000000000001e18;
 
     function setUp() public virtual override {
         super.setUp();
 
-        vm.prank(creator);
-        eulerSwapFactory = new EulerSwapFactory(IPoolManager(address(0)), address(evc));
+        vm.startPrank(creator);
+        poolManager = PoolManagerDeployer.deploy(creator);
+        eulerSwapFactory = new EulerSwapFactory(poolManager, address(evc));
+        vm.stopPrank();
     }
 
     function testDeployPool() public {
         uint256 allPoolsLengthBefore = eulerSwapFactory.allPoolsLength();
 
-        bytes32 salt = bytes32(uint256(1234));
         IEulerSwap.Params memory poolParams = IEulerSwap.Params(address(eTST), address(eTST2), holder, 1e18, 1e18, 0);
         IEulerSwap.CurveParams memory curveParams = IEulerSwap.CurveParams(0.4e18, 0.85e18, 1e18, 1e18);
 
+        uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG);
+        bytes memory constructorArgs = abi.encode(poolManager, poolParams, curveParams);
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(address(eulerSwapFactory), holder, flags, type(EulerSwapHook).creationCode, constructorArgs);
+
         address predictedAddress = predictPoolAddress(address(eulerSwapFactory), poolParams, curveParams, salt);
+        assertEq(hookAddress, predictedAddress);
 
         IEVC.BatchItem[] memory items = new IEVC.BatchItem[](2);
 
@@ -54,12 +66,18 @@ contract EulerSwapFactoryTest is EulerSwapTestBase {
         assertEq(poolsList[0], address(eulerSwap));
         assertEq(eulerSwapFactory.allPools(0), address(eulerSwap));
 
+        // revert when attempting to deploy a new pool (with a different salt)
+        (address newHookAddress, bytes32 newSalt) =
+            HookMiner.find(address(eulerSwapFactory), holder, flags, type(EulerSwapHook).creationCode, constructorArgs);
+        assertNotEq(newHookAddress, hookAddress);
+        assertNotEq(newSalt, salt);
+
         items = new IEVC.BatchItem[](1);
         items[0] = IEVC.BatchItem({
             onBehalfOfAccount: holder,
             targetContract: address(eulerSwapFactory),
             value: 0,
-            data: abi.encodeCall(EulerSwapFactory.deployPool, (poolParams, curveParams, bytes32(uint256(12345))))
+            data: abi.encodeCall(EulerSwapFactory.deployPool, (poolParams, curveParams, newSalt))
         });
 
         vm.prank(holder);
@@ -97,7 +115,7 @@ contract EulerSwapFactoryTest is EulerSwapTestBase {
         IEulerSwap.Params memory poolParams,
         IEulerSwap.CurveParams memory curveParams,
         bytes32 salt
-    ) internal pure returns (address) {
+    ) internal view returns (address) {
         return address(
             uint160(
                 uint256(
@@ -107,7 +125,9 @@ contract EulerSwapFactoryTest is EulerSwapTestBase {
                             factoryAddress,
                             keccak256(abi.encode(address(poolParams.eulerAccount), salt)),
                             keccak256(
-                                abi.encodePacked(type(EulerSwap).creationCode, abi.encode(poolParams, curveParams))
+                                abi.encodePacked(
+                                    type(EulerSwapHook).creationCode, abi.encode(poolManager, poolParams, curveParams)
+                                )
                             )
                         )
                     )
