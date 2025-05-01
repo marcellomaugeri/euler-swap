415ce5bb6b600561f433d2413227d681707a57b1
Michael Bentley <michael@euler.xyz>
2025-05-02 00:01:00 +0100

    chore: use ceil-div in f() and fInverse()

--- src/libraries/CurveLib.sol
+++ src/libraries/CurveLib.sol
@@ -35,7 +35,7 @@ library CurveLib {
         unchecked {
             uint256 v = Math.mulDiv(px * (x0 - x), c * x + (1e18 - c) * x0, x * 1e18, Math.Rounding.Ceil);
             require(v <= type(uint248).max, Overflow());
-            return y0 + (v + (py - 1)) / py;
+            return y0 + Math.ceilDiv(v, py);
         }
     }
 
@@ -85,7 +85,7 @@ library CurveLib {
             x = Math.mulDiv(absB + sqrt, 1e18, 2 * c, Math.Rounding.Ceil) + 1;
         } else {
             // use the "citardauq" quadratic formula solution 2c / (-b - sqrt(b^2 - 4ac))
-            x = (2 * C + (absB + sqrt - 1)) / (absB + sqrt) + 1;
+            x = Math.ceilDiv(2 * C, absB + sqrt) + 1;
         }
 
         if (x >= x0) {
