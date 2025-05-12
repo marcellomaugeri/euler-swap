f407708cb41f6098555d97d04cceda9d8c7c2132
saucepoint <saucepoint@protonmail.com>
2025-05-12 16:12:29 -0400

    specify min/max protocol fee, allow for permissionless protocol fee enablement

--- src/utils/ProtocolFee.sol
+++ src/utils/ProtocolFee.sol
@@ -6,16 +6,28 @@ import {Owned} from "solmate/src/auth/Owned.sol";
 abstract contract ProtocolFee is Owned {
     uint256 public protocolFee;
     address public protocolFeeRecipient;
+    uint256 public immutable deploymentTimestamp;
+
+    uint256 public constant MIN_PROTOCOL_FEE = 0.1e18; // 10%
+    uint256 public constant MAX_PROTOCOL_FEE = 0.25e18; // 25%
 
     error InvalidFee();
     error RecipientSetAlready();
 
-    constructor(address _feeOwner) Owned(_feeOwner) {}
+    constructor(address _feeOwner) Owned(_feeOwner) {
+        deploymentTimestamp = block.timestamp;
+    }
+
+    /// @notice Permissionlessly enable a minimum protocol fee after 1 year
+    function enableProtocolFee() external {
+        require(block.timestamp >= (deploymentTimestamp + 365 days) && protocolFeeRecipient != address(0), InvalidFee());
+        protocolFee = MIN_PROTOCOL_FEE;
+    }
 
     /// @notice Set the protocol fee, expressed as a percentage of LP fee
     /// @param newFee The new protocol fee, in WAD units (0.10e18 = 10%)
     function setProtocolFee(uint256 newFee) external onlyOwner {
-        require(newFee <= 0.25e18 && protocolFeeRecipient != address(0), InvalidFee());
+        require(MIN_PROTOCOL_FEE <= newFee && newFee <= 0.25e18 && protocolFeeRecipient != address(0), InvalidFee());
         protocolFee = newFee;
     }
 
